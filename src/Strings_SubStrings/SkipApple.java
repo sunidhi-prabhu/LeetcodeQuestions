7361c02b9b63d09d687ee497f6ab50841f69ba9f
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2023-12-06 07:37:17 +0530

    initial commit

--- src/Strings_SubStrings/SkipApple.java
+++ src/Strings_SubStrings/SkipApple.java
@@ -4,16 +4,16 @@ package Strings_SubStrings;
 
 public class SkipApple {
     public static void main(String[] args) {
-        String ans = skipApple("abcdapplee");
+        String ans = skipAppnotapple("abcappledapp");//call the required the function
         System.out.println(ans);
     }
 
-    static String skipApple(String up){
-        if(up.isEmpty())
+    static String skipApple(String up) {
+        if (up.isEmpty())
             return ("");
         char ch = up.charAt(0);
 
-        if(up.startsWith("apple"))//any required string that is to be removed can be passed here
+        if (up.startsWith("apple"))//any required string that is to be removed can be passed here
             return skipApple(up.substring(5));
 
         else
@@ -21,4 +21,17 @@ public class SkipApple {
     }
 
 
+    static String skipAppnotapple(String up) {
+        if (up.isEmpty())
+            return ("");
+        char ch = up.charAt(0);
+
+        if (up.startsWith("app") && !up.startsWith("apple") )//any required string that is to be removed can be passed here
+            return skipApple(up.substring(3));
+
+        else
+            return up.charAt(0) + skipAppnotapple(up.substring(1));
+    }
 }
+
+
