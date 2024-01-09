0dcc231e7a0a1b6df02923c8de470bf1273c791e
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2024-01-09 07:02:36 +0530

    initial commit

--- src/ReduceToZero.java
+++ src/ReduceToZero.java
@@ -1,19 +1,23 @@
+//Given an integer n, return the number of steps to reduce it to zero.
 public class ReduceToZero {
     public static void main(String[] args) {
         int n = 123, count =0;
         reduce(n,count);
     }
-
     private static void reduce(int n,int count) {
-        if(n==0) {
+
+        if(n==0)
+        {
             System.out.println(count);
             return;
         }
-        if(n % 2 == 0) {
+        if(n % 2 == 0)
+        {
             n /= 2;
             count++;
         }
-        else {
+        else
+        {
             n -= 1;
             count++;
         }
