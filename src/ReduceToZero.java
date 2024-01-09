ee529c178817e4e449896fd03996e265129f900d
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2024-01-09 07:01:43 +0530

    initial commit

---
+++ src/ReduceToZero.java
@@ -0,0 +1,22 @@
+public class ReduceToZero {
+    public static void main(String[] args) {
+        int n = 123, count =0;
+        reduce(n,count);
+    }
+
+    private static void reduce(int n,int count) {
+        if(n==0) {
+            System.out.println(count);
+            return;
+        }
+        if(n % 2 == 0) {
+            n /= 2;
+            count++;
+        }
+        else {
+            n -= 1;
+            count++;
+        }
+        reduce(n,count);
+    }
+}
